170e1fed89cdc002cf2bd9c291768f2cc680b17c
MinhTri <dhmt721305@gmail.com>
2025-04-01 10:22:34 +0700

    Update logic

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -17,6 +17,7 @@ import {
 export async function createQuestion(
   params: CreateQuestionParams
 ): Promise<ActionResponse<Question>> {
+  console.log("params", params);
   const validationResult = await action({
     params,
     schema: AskQuestionSchema,
@@ -27,6 +28,7 @@ export async function createQuestion(
     return handleError(validationResult) as ErrorResponse;
   }
 
+  console.log("validationResult", validationResult);
   const { title, content, tags } = validationResult.params!;
   const userId = validationResult?.session?.user?.id;
 
@@ -52,7 +54,7 @@ export async function createQuestion(
         { $setOnInsert: { name: tag }, $inc: { questions: 1 } },
         { upsert: true, new: true, session }
       );
-
+      console.log("existingTag", existingTag);
       tagIds.push(existingTag._id);
       tagQuestionDocuments.push({
         tag: existingTag._id,
@@ -100,38 +102,41 @@ export async function editQuestion(
 
   try {
     const question = await Question.findById(questionId).populate("tags");
-
+    console.log("question populated", question);
     if (!question) {
       throw new Error("Question not found");
     }
 
     if (question.author.toString() !== userId) {
-      throw new Error("Unauthorized");
+      throw new Error("Unauthorize");
     }
 
-    if (question.title !== title || question.content !== content) {
+    if (question.title! == title || question.content !== content) {
       question.title = title;
       question.content = content;
+
+      // Save
       await question.save({ session });
     }
 
-    const tagsToAdd = tags.filter(
-      (tag) => !question.tags.includes(tag.toLowerCase())
+    // Find new Tag
+    const tagsAdd = tags.filter(
+      (tag) => !question.tags.includes(tag.toLocaleLowerCase())
     );
+    // Find tag remove
     const tagsToRemove = question.tags.filter(
       (tag: ITagDoc) => !tags.includes(tag.name.toLowerCase())
     );
 
     const newTagDocuments = [];
-
-    if (tagsToAdd.length > 0) {
-      for (const tag of tagsToAdd) {
+    // Add Tags
+    if (tagsAdd.length > 0) {
+      for (const tag of tagsAdd) {
         const existingTag = await Tag.findOneAndUpdate(
           { name: { $regex: new RegExp(`^${tag}$`, "i") } },
           { $setOnInsert: { name: tag }, $inc: { questions: 1 } },
           { upsert: true, new: true, session }
         );
-
         if (existingTag) {
           newTagDocuments.push({
             tag: existingTag._id,
@@ -142,18 +147,27 @@ export async function editQuestion(
         }
       }
     }
-
+    // Remove Tags
     if (tagsToRemove.length > 0) {
-      const tagIdsToRemove = tagsToRemove.map((tag: ITagDoc) => tag._id);
+      const tagIdsRemove = tagsToRemove.map((tag: ITagDoc) => tag._id);
 
       await Tag.updateMany(
-        { _id: { $in: tagIdsToRemove } },
-        { $inc: { questions: -1 } },
-        { session }
+        {
+          _id: { $in: tagIdsRemove },
+        },
+        {
+          $inc: { questions: -1 },
+        },
+        {
+          session,
+        }
       );
 
+      // Delete tags where question == questionId and tag includes tagIdsRemove
       await TagQuestion.deleteMany(
-        { tag: { $in: tagIdsToRemove }, question: questionId },
+        {
+          tag: { $in: tagIdsRemove, question: questionId },
+        },
         { session }
       );
 
@@ -174,9 +188,108 @@ export async function editQuestion(
     await session.abortTransaction();
     return handleError(error) as ErrorResponse;
   } finally {
-    await session.endSession();
+    session.endSession();
   }
 }
+
+// export async function editQuestion(
+//   params: EditQuestionParams
+// ): Promise<ActionResponse<Question>> {
+//   const validationResult = await action({
+//     params,
+//     schema: EditQuestionSchema,
+//     authorize: true,
+//   });
+
+//   if (validationResult instanceof Error) {
+//     return handleError(validationResult) as ErrorResponse;
+//   }
+
+//   const { title, content, tags, questionId } = validationResult.params!;
+//   const userId = validationResult?.session?.user?.id;
+
+//   const session = await mongoose.startSession();
+//   session.startTransaction();
+
+//   try {
+//     const question = await Question.findById(questionId).populate("tags");
+
+//     if (!question) {
+//       throw new Error("Question not found");
+//     }
+
+//     if (question.author.toString() !== userId) {
+//       throw new Error("Unauthorized");
+//     }
+
+//     if (question.title !== title || question.content !== content) {
+//       question.title = title;
+//       question.content = content;
+//       await question.save({ session });
+//     }
+
+//     const tagsToAdd = tags.filter(
+//       (tag) => !question.tags.includes(tag.toLowerCase())
+//     );
+//     const tagsToRemove = question.tags.filter(
+//       (tag: ITagDoc) => !tags.includes(tag.name.toLowerCase())
+//     );
+
+//     const newTagDocuments = [];
+
+//     if (tagsToAdd.length > 0) {
+//       for (const tag of tagsToAdd) {
+//         const existingTag = await Tag.findOneAndUpdate(
+//           { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+//           { $setOnInsert: { name: tag }, $inc: { questions: 1 } },
+//           { upsert: true, new: true, session }
+//         );
+
+//         if (existingTag) {
+//           newTagDocuments.push({
+//             tag: existingTag._id,
+//             question: questionId,
+//           });
+
+//           question.tags.push(existingTag._id);
+//         }
+//       }
+//     }
+
+//     if (tagsToRemove.length > 0) {
+//       const tagIdsToRemove = tagsToRemove.map((tag: ITagDoc) => tag._id);
+
+//       await Tag.updateMany(
+//         { _id: { $in: tagIdsToRemove } },
+//         { $inc: { questions: -1 } },
+//         { session }
+//       );
+
+//       await TagQuestion.deleteMany(
+//         { tag: { $in: tagIdsToRemove }, question: questionId },
+//         { session }
+//       );
+
+//       question.tags = question.tags.filter(
+//         (tagId: mongoose.Types.ObjectId) => !tagsToRemove.includes(tagId)
+//       );
+//     }
+
+//     if (newTagDocuments.length > 0) {
+//       await TagQuestion.insertMany(newTagDocuments, { session });
+//     }
+
+//     await question.save({ session });
+//     await session.commitTransaction();
+
+//     return { success: true, data: JSON.parse(JSON.stringify(question)) };
+//   } catch (error) {
+//     await session.abortTransaction();
+//     return handleError(error) as ErrorResponse;
+//   } finally {
+//     await session.endSession();
+//   }
+// }
 
 export async function getQuestion(
   params: GetQuestionParams
