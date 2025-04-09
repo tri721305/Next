6ba303907013f3f95756364ab14fed022ac4c6e9
MinhTri <dhmt721305@gmail.com>
2025-04-09 15:11:57 +0700

    Feature: Tags manager

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,15 +1,48 @@
+import TagCard from "@/components/cards/TagCard";
+import DataRenderer from "@/components/DataRenderer";
+import LocalSearch from "@/components/search/LocalSearch";
+import ROUTES from "@/constants/routes";
+import { EMPTY_TAGS } from "@/constants/states";
 import { getTags } from "@/lib/actions/tag.actions";
 import React from "react";
 
-const Tags = async () => {
+const Tags = async ({ searchParams }: RouteParams) => {
+  const { page, pageSize, query, filter } = await searchParams;
   const { success, data, error } = await getTags({
     page: 1,
     pageSize: 10,
     // query: "back",
+    query: query,
+    filter: filter,
   });
   const { tags } = data || {};
   console.log("Tags", JSON.stringify(tags, null, 2));
-  return <div>Tags</div>;
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900 text-3xl">Tags</h1>
+      <section className="mt-11">
+        <LocalSearch
+          route={ROUTES.TAGS}
+          imgSrc="/icons/search.svg"
+          placeholder="Search tags ..."
+          otherClasses="flex-1"
+        />
+      </section>
+      <DataRenderer
+        success={success}
+        error={error}
+        data={tags}
+        empty={EMPTY_TAGS}
+        render={(tags) => (
+          <div className="mt-10 flex w-full flex-wrap gap-4">
+            {tags.map((tag) => (
+              <TagCard key={tag._id} {...tag} compact={false} />
+            ))}
+          </div>
+        )}
+      />
+    </>
+  );
 };
 
 export default Tags;
