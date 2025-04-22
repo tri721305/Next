e68ead420a3207e4394e88f9b9a9efc908681aac
MinhTri <dhmt721305@gmail.com>
2025-04-22 21:58:48 +0700

    Update logic vote

--- components/cards/AnswerCard.tsx
+++ components/cards/AnswerCard.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { Suspense } from "react";
 import UserAvatar from "../UserAvatar";
 import Link from "next/link";
 import ROUTES from "@/constants/routes";
 import Preview from "../editor/Preview";
+import Votes from "../votes/Votes";
+import { hasVoted } from "@/lib/actions/vote.action";
 
-const AnswerCard = ({ _id, author, content, createdAt }: Answer) => {
+const AnswerCard = ({
+  _id,
+  author,
+  content,
+  createdAt,
+  upvotes,
+  downvotes,
+}: Answer) => {
+  const hasVotedPromise = hasVoted({
+    targetId: _id,
+    targetType: "answer",
+  });
+
   return (
     <article className="light-border border-b py-10">
       <span id={JSON.stringify(_id)} className="hash-span"></span>
@@ -25,7 +39,17 @@ const AnswerCard = ({ _id, author, content, createdAt }: Answer) => {
           </Link>
         </div>
 
-        <div className="flex justify-end">Votes</div>
+        <div className="flex justify-end">
+          <Suspense fallback={<div>Loading ...</div>}>
+            <Votes
+              targetType="answer"
+              upvotes={upvotes}
+              downvotes={downvotes}
+              hasVotedPromise={hasVotedPromise}
+              targetId={_id}
+            />
+          </Suspense>
+        </div>
       </div>
       <Preview content={content} />
     </article>
