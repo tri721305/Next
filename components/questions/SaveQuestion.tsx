999089f3afbd06bec496a2968fa88babd5b512a7
MinhTri <dhmt721305@gmail.com>
2025-04-23 13:36:40 +0700

    Implement safe question

---
+++ components/questions/SaveQuestion.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { toast } from "@/hooks/use-toast";
+import { toggleSaveQuestion } from "@/lib/actions/collection.action";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useState } from "react";
+
+const SaveQuestion = ({ questionId }: { questionId: string }) => {
+  const session = useSession();
+
+  const userId = session?.data?.user?.id;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSave = async () => {
+    if (isLoading) return;
+    if (!userId)
+      return toast({
+        title: "You need to be logged in to save a question",
+        variant: "destructive",
+      });
+    setIsLoading(true);
+
+    try {
+      const { success, data, error } = await toggleSaveQuestion({ questionId });
+
+      if (!success) throw new Error(error?.message || "An error occurred");
+      toast({
+        title: `Question ${data?.saved ? "saved" : "unsaved"} successfully`,
+        // variant: data?.saved ? "default" : "destructive",
+      });
+    } catch (error) {
+      toast({
+        title: "Error",
+        description:
+          error instanceof Error ? error.message : "An error occurred",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const hasSaved = false;
+  return (
+    <Image
+      src={hasSaved ? "/icons/star-filled.svg" : "/icons/star-red.svg"}
+      width={18}
+      height={18}
+      alt="Save"
+      className={`cursor-pointer ${isLoading && "opacity-50"}`}
+      onClick={handleSave}
+    />
+  );
+};
+
+export default SaveQuestion;
